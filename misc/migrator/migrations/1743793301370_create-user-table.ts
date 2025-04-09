8037813ce838491a81d7c845849b5e2be17367d3
Imtiaz Ahmed Khan <ak.imtiaz147@gmail.com>
2025-04-09 13:53:08 +0600

    added some migration files

--- misc/migrator/migrations/1743793301370_create-user-table.ts
+++ misc/migrator/migrations/1743793301370_create-user-table.ts
@@ -2,11 +2,14 @@ import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
-// CREATE TABLE users
-// (
-//   id SERIAL PRIMARY KEY,
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY NOT NULL,
 //   name VARCHAR(255) NOT NULL,
-//   email VARCHAR(255) NOT NULL UNIQUE
+//   email VARCHAR(255) NOT NULL UNIQUE,
+//   profile_picture VARCHAR(255),
+//   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+//   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+//   deleted_at TIMESTAMP
 // );
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
@@ -25,7 +28,27 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
       notNull: true,
       unique: true,
     },
+    profile_picture: {
+      type: "varchar(255)",
+      notNull: false,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    deleted_at: {
+      type: "timestamp",
+      notNull: false,
+    },
   });
 }
 
-export async function down(pgm: MigrationBuilder): Promise<void> {}
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("users");
+}
